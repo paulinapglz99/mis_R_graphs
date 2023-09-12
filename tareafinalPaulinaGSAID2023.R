46d57f6ea4394079faa1fd02789c3b411e989776
Alejandra Paulina Pérez González <82723700+paulinapglz99@users.noreply.github.com>
2023-09-12 00:45:52 -0600

    Add files via upload

--- tareafinalPaulinaGSAID2023.R
+++ tareafinalPaulinaGSAID2023.R
@@ -1,10 +1,12 @@
-#Tarea evaluativa curso RDataScience
+#Tarea evaluativa curso RDataScience 2023
 #Alejandra Paulina Perez Gonzalez - paulinapglz.99@gmail.com
 
 
 #Usar datos de gsaid_2023.txt
+
 #Mision: 
 #Analisis de variantes genomicas de SARS-CoV2. Las preguntas son:
+
 #1)Que pais ha hecho mas secuenciacion de SARS en 2023?
 #2) En que mes se ha hecho mas secuenciacion? 
 #3) Cual(es) es la variante(s) dominante(s) en cada mes por pais?
@@ -34,8 +36,9 @@ gsaid2023 <- vroom(file = 'https://raw.githubusercontent.com/INMEGEN/CienciaDeDa
 #variant under monitoring (VUM), variant of interest (VOI) and variant of concern (VOC).
 
 gsaid2023 <- gsaid2023 %>% 
-  mutate(annum = year(collection_date), 
-         month = month(collection_date))#usar lubridate para crear la columna de anio y mes
+  mutate(anio = year(collection_date), 
+         month = month(collection_date)) %>% #usar lubridate para crear la columna de anio y mes
+         select(-collection_date) #eliminar col repetida
 
 gsaid2023 <- gsaid2023 %>% 
   separate_wider_delim(cols = variant,
@@ -45,9 +48,8 @@ gsaid2023 <- gsaid2023 %>%
 #eliminar el remanente
 
 gsaid2023$first_detected_in <- str_replace_all(string = gsaid2023$first_detected_in,
-                  pattern = "cted in",
+                  pattern = "cted in ",
                   replace = "")
-
 
 #1)Que pais ha hecho mas secuenciacion de SARS en 2023?
 
@@ -62,6 +64,37 @@ conteo_seq.p <- conteo_seq %>%
              y =  count)) +       #mi eje y sera
   geom_col()
 
+#vis del plot
+
+conteo_seq.p
 
 #el pais que ha hecho mas secuenciacion de SARS-CoV2 en 2023 es
 #USA
+
+#2) En que mes se ha hecho mas secuenciacion? En enero jej
+
+conteo_seq_month <- gsaid2023 %>% 
+  group_by(month) %>%     #los agrupamos y sumamos por pais y conteo
+  summarise(count = n())
+
+#ploteando
+
+conteo_seq_month.p <- conteo_seq_month %>% 
+  ggplot(aes(x = month,   #mi eje x sera 
+             y =  count)) +       #mi eje y sera
+  geom_col()
+
+
+#3) Cual(es) es la variante(s) dominante(s) en cada mes por pais? este si va a estar mas perru
+
+conteo_variante_month_country <- gsaid2023 %>% 
+  group_by(month, type_of_variant, country) %>%     #los agrupamos y sumamos por pais y conteo
+  summarise(count = n())
+
+#ploteando
+
+conteo_variante_month_country.p <- conteo_variante_month_country %>% 
+  ggplot(aes(x = month,   #mi eje x sera 
+             y =  count, 
+             fill = type_of_variant)) +       #mi eje y sera
+  geom_col()
